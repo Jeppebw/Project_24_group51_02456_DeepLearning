a480fc3e3e7528fa31954f2dbd46356c6b1a6f39
jeppebw <worm.jeppe@gmail.com>
2024-11-25 16:33:42 +0100

    added another mamba and embedding script

--- models/Mambamodel.py
+++ models/Mambamodel.py
@@ -8,8 +8,9 @@ from torch.cuda.amp import autocast
 from torch.optim import AdamW
 from torch.optim.lr_scheduler import CosineAnnealingLR, LinearLR, SequentialLR
 from torch.nn.functional import gelu
-from transformers import Mamba2Config, Mamba2ForCausalLM
+from transformers import MambaConfig, MambaForCausalLM
 from transformers.activations import ACT2FN
+from models import MambaEmbeddingsForCEHR
 
 class MambaClassificationHead(nn.Module):
     """Head for sentence-level classification tasks."""
@@ -40,40 +41,40 @@ class MambaClassificationHead(nn.Module):
 
         return self.out_proj(x)
 
+############ Mamba model 1 #############
 
-class EHRMamba2(nn.Module):
-    """Mamba2 model"""
+class EHRmamba(nn.Module):
+    """Mamba model for pretraining."""
 
     def __init__(
         self,
         vocab_size: int,
-        embedding_size: int = 137, # Was 768, now 137
+        embedding_size: int = 768,
+        time_embeddings_size: int = 32,
+        visit_order_size: int = 3,
+        type_vocab_size: int = 9,
         max_num_visits: int = 512,
         max_seq_length: int = 2048,
-        state_size: int = 64,
-        num_heads: int = 24,
-        head_dim: int = 64,
+        state_size: int = 16,
         num_hidden_layers: int = 32,
         expand: int = 2,
         conv_kernel: int = 4,
         learning_rate: float = 5e-5,
         dropout_prob: float = 0.1,
         padding_idx: int = 0,
-        cls_idx: int = 1,  # used as bos token
-        eos_idx: int = 2,
-        n_groups: int = 1,
-        chunk_size: int = 256,
-        num_classes: int = 2,  # Number of output classes
+        cls_idx: int = 5,
+        use_mambapy: bool = False,
     ):
         super().__init__()
 
         self.vocab_size = vocab_size
         self.embedding_size = embedding_size
+        self.time_embeddings_size = time_embeddings_size
+        self.visit_order_size = visit_order_size
+        self.type_vocab_size = type_vocab_size
         self.max_num_visits = max_num_visits
         self.max_seq_length = max_seq_length
         self.state_size = state_size
-        self.num_heads = num_heads
-        self.head_dim = head_dim
         self.num_hidden_layers = num_hidden_layers
         self.expand = expand
         self.conv_kernel = conv_kernel
@@ -81,11 +82,9 @@ class EHRMamba2(nn.Module):
         self.dropout_prob = dropout_prob
         self.padding_idx = padding_idx
         self.cls_idx = cls_idx
-        self.eos_idx = eos_idx
-        self.n_groups = n_groups
-        self.chunk_size = chunk_size
+        self.use_mambapy = use_mambapy
 
-        self.config = Mamba2Config(
+        self.config = MambaConfig(
             vocab_size=self.vocab_size,
             hidden_size=self.embedding_size,
             state_size=self.state_size,
@@ -94,112 +93,133 @@ class EHRMamba2(nn.Module):
             conv_kernel=self.conv_kernel,
             pad_token_id=self.padding_idx,
             bos_token_id=self.cls_idx,
-            eos_token_id=self.eos_idx,
-            n_groups=self.n_groups,
-            chunk_size=self.chunk_size,
-            dropout=self.dropout_prob,
-            num_heads=self.num_heads,
-            head_dim=self.head_dim,
-            max_position_embeddings=self.max_seq_length,
+            eos_token_id=self.padding_idx,
+            use_mambapy=self.use_mambapy,
         )
+        self.embeddings = MambaEmbeddingsForCEHR(
+            config=self.config,
+            type_vocab_size=self.type_vocab_size,
+            max_num_visits=self.max_num_visits,
+            time_embeddings_size=self.time_embeddings_size,
+            visit_order_size=self.visit_order_size,
+            hidden_dropout_prob=self.dropout_prob,
+        )
+        # Initialize weights and apply final processing
+        self.post_init()
 
         # Mamba has its own initialization
-        self.model = Mamba2ForCausalLM(config=self.config)
-        
-        # Classification head
-        self.classification_head = MambaClassificationHead(
-            hidden_size=embedding_size,
-            classifier_dropout=dropout_prob,
-            num_labels=num_classes,
-            hidden_act="gelu",
-        )
+        self.model = MambaForCausalLM(config=self.config)
 
-    def forward(self, concept_ids, labels=None, attention_mask=None):
-        """Forward pass through the model and classification head."""
-        # Ensure input tensor is of type LongTensor
-        concept_ids = concept_ids.long()
-        
-        print(f"Input ID range: min={concept_ids.min().item()}, max={concept_ids.max().item()}")
-        print(f"Embedding vocab size: {self.embedding_size}")
-        
-        ##### Error ###########
-        # Input_id range is -7 to 8. It is supposed to be between 0 and vocab_size (768) - 1
-        # Does negative values mean something or can you just set them to zero?
-        concept_ids = torch.abs(concept_ids)
-        
-        # Debug shapes of inputs
-        print(f"concept_ids shape: {concept_ids.shape}")
-        print(f"labels shape: {labels.shape if labels is not None else None}")
-        print(f"attention_mask shape: {attention_mask.shape if attention_mask is not None else None}")
+    def _init_weights(self, module: torch.nn.Module) -> None:
+        """Initialize the weights."""
+        if isinstance(module, nn.Linear):
+            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+            if module.bias is not None:
+                module.bias.data.zero_()
+        elif isinstance(module, nn.Embedding):
+            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+            if module.padding_idx is not None:
+                module.weight.data[module.padding_idx].zero_()
+        elif isinstance(module, nn.LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
 
-        # Adjust attention_mask if needed
-        if attention_mask is not None and attention_mask.shape[2] != concept_ids.shape[2]:
-            seq_length = concept_ids.shape[2]
-            attention_mask = attention_mask[:, :seq_length]
-            print(f"Adjusted attention_mask shape: {attention_mask.shape}")
-        
-        outputs = self.model(
+    def post_init(self) -> None:
+        """Apply weight initialization."""
+        self.apply(self._init_weights)
+
+    def forward(
+        self,
+        inputs: Tuple[
+            torch.Tensor,
+            torch.Tensor,
+            torch.Tensor,
+            torch.Tensor,
+            torch.Tensor,
+            torch.Tensor,
+        ],
+        labels: Optional[torch.Tensor] = None,
+        output_hidden_states: Optional[bool] = False,
+        return_dict: Optional[bool] = True,
+    ) -> Union[Tuple[torch.Tensor, ...], MambaCausalLMOutput]:
+        """Forward pass for the model."""
+        concept_ids, type_ids, time_stamps, ages, visit_orders, visit_segments = inputs
+        inputs_embeds = self.embeddings(
             input_ids=concept_ids,
-            labels=labels,
-            attention_mask=attention_mask,
-            return_dict=True,
-            output_hidden_states=True,  # Ensure hidden states are returned
+            token_type_ids_batch=type_ids,
+            time_stamps=time_stamps,
+            ages=ages,
+            visit_orders=visit_orders,
+            visit_segments=visit_segments,
         )
-        
-        # Use the hidden state of the [CLS] token for classification
-        hidden_states = outputs.hidden_states[-1]  # Last layer's hidden states
-        
-        # Debugging: Log the shape of hidden_states
-        print(f"Hidden states shape: {hidden_states.shape}")
-        
-        cls_token_state = hidden_states[:, 0, :]  # Assuming the first token is [CLS]
-        
-        # Debugging: Log the shape of the [CLS] token state
-        print(f"CLS token state shape: {cls_token_state.shape}")
 
-        # Pass through classification head
-        logits = self.classification_head(cls_token_state)
+        if labels is None:
+            labels = concept_ids
 
-        return outputs.loss, logits
+        return self.model(
+            inputs_embeds=inputs_embeds,
+            labels=labels,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
 
-    def _step(self, batch: Dict[str, Any], batch_idx: int, stage: str) -> Any:
-        """Run a single step for training or validation.
-
-        Args:
-            batch: Input batch dictionary
-            batch_idx: Index of current batch
-            stage: Either 'train' or 'val'
-        """
-        concept_ids = batch["concept_ids"]
+    def training_step(self, batch: Dict[str, Any], batch_idx: int) -> Any:
+        """Train model on training dataset."""
+        inputs = (
+            batch["concept_ids"],
+            batch["type_ids"],
+            batch["time_stamps"],
+            batch["ages"],
+            batch["visit_orders"],
+            batch["visit_segments"],
+        )
         labels = batch["labels"]
-        attention_mask = batch["attention_mask"]
 
         # Ensure use of mixed precision
         with autocast():
-            loss = self.model(
-                concept_ids,
+            loss = self(
+                inputs,
                 labels=labels,
-                attention_mask=attention_mask,
                 return_dict=True,
-                output_hidden_states=False,
+            ).loss
+
+        (current_lr,) = self.lr_schedulers().get_last_lr()
+        self.log_dict(
+            dictionary={"train_loss": loss, "lr": current_lr},
+            on_step=True,
+            prog_bar=True,
+            sync_dist=True,
+        )
+        return loss
+
+    def validation_step(self, batch: Dict[str, Any], batch_idx: int) -> Any:
+        """Evaluate model on validation dataset."""
+        inputs = (
+            batch["concept_ids"],
+            batch["type_ids"],
+            batch["time_stamps"],
+            batch["ages"],
+            batch["visit_orders"],
+            batch["visit_segments"],
+        )
+        labels = batch["labels"]
+
+        # Ensure use of mixed precision
+        with autocast():
+            loss = self(
+                inputs,
+                labels=labels,
+                return_dict=True,
             ).loss
 
         (current_lr,) = self.lr_schedulers().get_last_lr()
         #self.log_dict(
-        #    dictionary={f"{stage}_loss": loss, "lr": current_lr},
+        #    dictionary={"val_loss": loss, "lr": current_lr},
         #    on_step=True,
         #    prog_bar=True,
         #    sync_dist=True,
         #)
         return loss
-
-    def training_step(self, batch: Dict[str, Any], batch_idx: int) -> Any:
-        """Train model on training dataset."""
-        return self._step(batch, batch_idx, "train")
-
-    def validation_step(self, batch: Dict[str, Any], batch_idx: int) -> Any:
-        """Evaluate model on validation dataset."""
-        return self._step(batch, batch_idx, "val")
 
     def configure_optimizers(
         self,
@@ -220,10 +240,11 @@ class EHRMamba2(nn.Module):
             end_factor=1.0,
             total_iters=n_warmup_steps,
         )
-        decay = CosineAnnealingLR(
+        decay = LinearLR(
             optimizer,
-            T_max=n_decay_steps,
-            eta_min=self.learning_rate * 0.01,
+            start_factor=1.0,
+            end_factor=0.01,
+            total_iters=n_decay_steps,
         )
         scheduler = SequentialLR(
             optimizer=optimizer,
